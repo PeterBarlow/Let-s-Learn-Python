240b32fb37b60cd4af02848726258c876f16f2d2
PeterBarlow <peterruelbarlow@gmail.com>
2015-09-22 16:18:23 +0200

    3

--- Newb.py
+++ Newb.py
@@ -1,6 +1,1 @@
-favorite_sports = {'Ralph Williams' : 'Football', 
-				'Michael Tippertt' : 'Basketball', 
-				'Edward Elgar' : 'Baseball', 
-				'Rebecca Clarke' : 'Netball', 
-				'Ethel Smyth' : 'Badminton',
-				'Frank Bridge' : 'Rugby'}
+foods = ['kbbq', 'Sushi']
